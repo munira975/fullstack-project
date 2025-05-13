4d538593bb9d19c1a55e4c76f01563e4330f4560
Sirajum Munira <muniramujaris975@gmail.com>
2025-05-13 21:23:04 +0200

    Mounted routes and removed connection details from server.js

--- server/server.js
+++ server/server.js
@@ -1,27 +1,15 @@
 import express from 'express';
-import mysql from 'mysql2';
 import dotenv from 'dotenv';
+import db from './db.js';
+import productRoutes from './routes/products.js'; 
 
-const app = express();
 dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use('/products', productRoutes);
 
 const PORT = process.env.PORT;
-
-const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'user',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'recipes'
-})
-
-db.connect(err => {
-    if (err) {
-        console.error('Failed to connect to MySQL:', err.message);
-    } else {
-        console.log(`Successfully connected to MySQL database.`)
-    };
-})
-
 app.listen(PORT, () => {
-    console.log(`Server is running on http//:localhost:${PORT}`)
-})
+    console.log(`Server is running on http://localhost:${PORT}`);
+});

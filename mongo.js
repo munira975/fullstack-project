d37bdd1fb1d65d7086fbc960a06b8d03f73ed6a6
Sirajum Munira <muniramujaris975@gmail.com>
2025-08-24 19:10:07 +0200

    Added mongo.js file to maintain the database connection

---
+++ mongo.js
@@ -0,0 +1,12 @@
+// server/mongo.js
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error("MongoDB connection error:", err));
